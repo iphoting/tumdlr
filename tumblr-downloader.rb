9fff8f011c81c94b6003f49a4cf4377a91cacf3d
Ronald Ip <myself@iphoting.com>
2015-03-01 00:38:59 +0800

    Don't crash on Exceptions.

--- tumblr-downloader.rb
+++ tumblr-downloader.rb
@@ -2,11 +2,15 @@ require 'nokogiri'
 require 'open-uri'
 
 def get_tumblr_vid_url (source = "")
-	iframe = Nokogiri::HTML(open(URI(source))) do |config|
-		config.noerror
-	end.at_css(".tumblr_video_container iframe[src]")['src']
+	begin
+		iframe = Nokogiri::HTML(open(URI(source))) do |config|
+			config.noerror
+		end.at_css(".tumblr_video_container iframe[src]")['src']
 
-	return Nokogiri::HTML(open(URI(iframe))) do |config|
-		config.noerror
-	end.at_css("video > source")['src']
+		return Nokogiri::HTML(open(URI(iframe))) do |config|
+			config.noerror
+		end.at_css("video > source")['src']
+	rescue
+		return nil
+	end
 end
